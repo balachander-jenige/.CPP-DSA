a27356b7965a5988464f10a89d8644bab9f8ae4d
Balachander Jenige <eduwithbala@gmail.com>
2025-12-31 07:15:52 +0800

    Time: 0 ms (100.00%) | Memory: 8.8 MB (86.74%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if( s[i]=='(' || s[i]=='{'|| s[i]=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.size()==0){
+                    return false;
+                }
+                char tp=st.top();
+                if(s[i]==')' && tp=='('){
+                    st.pop();
+                }
+                else if(s[i]==']' && tp=='['){
+                    st.pop();
+                }
+                else if(s[i]=='}' && tp=='{'){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+                
+
+            }
+        }
+        if(st.size()!=0){
+                    return false;
+                }
+        return true;
+    }
+};
