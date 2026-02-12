2b61a0df92f36150fd83160bb7e3a0bdb7350341
Balachander Jenige <eduwithbala@gmail.com>
2026-02-12 20:14:28 +0800

    Time: 1 ms (37.35%) | Memory: 8.1 MB (6.25%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
@@ -0,0 +1,46 @@
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+    l1 :=len(nums1)
+    l2 :=len(nums2)
+
+    var i,j int
+    var merged = []int{}
+    
+    for i < l1 && j < l2 {
+        if nums1[i] < nums2[j] {
+            merged = append(merged, nums1[i])
+            i++
+        } else {
+            merged = append(merged, nums2[j])
+            j++
+        }
+    }
+
+    // append remaining elements
+    for i < l1 {
+        merged = append(merged, nums1[i])
+        i++
+    }
+
+    for j < l2 {
+        merged = append(merged, nums2[j])
+        j++
+    }
+
+    index := len(merged)
+
+    if index%2 !=0 {
+        return float64(merged[index/2])
+    }else{
+        index:= index/2
+        return float64(float64(merged[index-1]+merged[index])/2.0)
+    }
+
+
+ }
+
+
+
+
+
+
+    
