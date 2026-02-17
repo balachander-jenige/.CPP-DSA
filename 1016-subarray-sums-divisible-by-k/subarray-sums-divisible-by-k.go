4cd693dbbf1dc0dc48e06ca8e7a652fed0440c6c
Balachander Jenige <eduwithbala@gmail.com>
2026-02-17 15:49:14 +0800

    Time: 13 ms (13.60%) | Memory: 8.5 MB (92.80%) - LeetSync

---
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.go
@@ -0,0 +1,25 @@
+func subarraysDivByK(nums []int, k int) int {
+
+      count :=0
+    prefixsum :=0
+    reminder := make(map[int]int)
+    reminder[0] = 1
+
+    for _, num := range nums {
+
+        prefixsum += num
+        rem := prefixsum % k 
+        if rem <0 {
+            rem = rem +k
+        }
+
+        if freq, exists := reminder[rem]; exists{
+            count += freq
+        }
+
+        reminder[rem % k] +=1
+    }    
+
+    return count
+    
+}
