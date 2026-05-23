b740a7efd8de6175ec59631d25a143d6c0d4ecd6
Balachander Jenige <eduwithbala@gmail.com>
2026-05-24 07:50:40 +0800

    Time: 21 ms (22.99%) | Memory: 10.4 MB (89.46%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.go
+++ 739-daily-temperatures/daily-temperatures.go
@@ -1,21 +1,38 @@
 func dailyTemperatures(temp []int) []int {
+
+    // Result array to store number of days until a warmer temperature
     res := make([]int, len(temp))
+
+    // Map to store last seen index of each temperature value
     m := make(map[int]int)
+
+    // Stack to store temperatures (not indexes in this version)
     st := []int{}
-    
-    for i:= len(temp)-1 ;i>=0; i--{
-        for len(st) >0  && temp[i] >= st[len(st)-1] { 
+
+    // Traverse the temperature array from right to left
+    for i := len(temp) - 1; i >= 0; i-- {
+
+        // Pop elements from stack while current temperature
+        // is greater or equal to stack top temperature
+        for len(st) > 0 && temp[i] >= st[len(st)-1] {
             st = st[:len(st)-1]
         }
-        if len(st) == 0{
-            res[i] = 0
 
-        }else{
-            res[i] = m[st[len(st)-1]] -i
+        // If no warmer temperature exists to the right
+        if len(st) == 0 {
+            res[i] = 0
+        } else {
+            // Use map to get index of next warmer temperature
+            // and compute difference in days
+            res[i] = m[st[len(st)-1]] - i
         }
-        st =append(st,temp[i])
+
+        // Push current temperature into stack
+        st = append(st, temp[i])
+
+        // Store index of current temperature
         m[temp[i]] = i
     }
+
     return res
-    
-} 
+}
