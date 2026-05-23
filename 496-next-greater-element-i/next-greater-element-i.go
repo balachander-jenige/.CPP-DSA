f681399044abaff9b7edd9570bf3d8643b85a6c2
Balachander Jenige <eduwithbala@gmail.com>
2026-05-24 06:59:08 +0800

    Time: 0 ms (100.00%) | Memory: 5.4 MB (6.18%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.go
+++ 496-next-greater-element-i/next-greater-element-i.go
@@ -1,28 +1,32 @@
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
 
     m := make(map[int]int)
-    res := []int{}
+    res := make([]int, len(nums2))
+    st := []int{}
+    finalres := []int{}
 
     // store index of nums2 elements
     for i, v := range nums2 {
         m[v] = i
     }
-
-    for _, v := range nums1 {
-
-        idx := m[v]
-        found := -1
-
-        // search to the right
-        for i := idx + 1; i < len(nums2); i++ {
-            if nums2[i] > v {
-                found = nums2[i]
-                break
-            }
+    res[len(nums2)-1] = -1
+    st= append(st,nums2[len(nums2)-1])
+    for i := len(nums2)-2 ; i>=0 ; i--{
+        for (len(st)!=0 && nums2[i] >= st[len(st)-1]){
+            st = st[:len(st)-1]
         }
-
-        res = append(res, found)
+        if (len(st)==0){
+            res[i]=-1
+        }else{
+            res[i] = st[len(st)-1]
+        }
+        st = append(st, nums2[i])
     }
 
-    return res
+   for _, value := range nums1{
+     i := m[value]
+     v1 := res[i]
+     finalres = append(finalres, v1)
+   }
+    return finalres
 }
