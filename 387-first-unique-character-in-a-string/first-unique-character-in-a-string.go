e461018ef75fd0134c57ba61ec12b238f8f7022f
Balachander Jenige <eduwithbala@gmail.com>
2026-05-25 23:39:18 +0800

    Time: 35 ms (25.64%) | Memory: 8.3 MB (15.22%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.go
@@ -0,0 +1,18 @@
+func firstUniqChar(s string) int {
+	d := map[byte]int{}
+
+	// Count each character.
+	for i := 0; i < len(s); i++ {
+		d[s[i]]++
+	}
+
+	// Find the first unique character and return.
+	for i := 0; i < len(s); i++ {
+		if d[s[i]] == 1 {
+			return i
+		}
+	}
+
+	// If there's no unique character then return -1.
+	return -1
+}
