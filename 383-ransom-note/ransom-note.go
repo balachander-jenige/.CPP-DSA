ee2fb2c985a9969129ab712a44c131133e283ac4
Balachander Jenige <eduwithbala@gmail.com>
2026-05-26 12:07:27 +0800

    Time: 11 ms (32.08%) | Memory: 5.9 MB (34.45%) - LeetSync

--- 383-ransom-note/ransom-note.go
+++ 383-ransom-note/ransom-note.go
@@ -1,14 +1,26 @@
 func canConstruct(ransomNote string, magazine string) bool {
 
-   m := make(map[rune]int)
-   for _,v := range magazine{
-    m[v] +=1
-   }
-   for _,v := range ransomNote{
-    m[v] -=1
-    if m[v] < 0{
-        return false
+    // Create a hashmap to store frequency of characters
+    m := make(map[rune]int)
+
+    // Count each character in magazine
+    for _, v := range magazine {
+        m[v]++
     }
-   }
-   return true
+
+    // Try to use characters for ransomNote
+    for _, v := range ransomNote {
+
+        // Decrease count because character is used
+        m[v]--
+
+        // If count becomes negative,
+        // magazine does not have enough characters
+        if m[v] < 0 {
+            return false
+        }
+    }
+
+    // All characters were available
+    return true
 }
