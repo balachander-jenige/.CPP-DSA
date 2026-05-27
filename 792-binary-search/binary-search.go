ddb24cb481dc4d1ff2f76a84c61ed10fcd04dea6
Balachander Jenige <eduwithbala@gmail.com>
2026-05-27 18:20:09 +0800

    Time: 0 ms (100.00%) | Memory: 8.4 MB (96.81%) - LeetSync

---
+++ 792-binary-search/binary-search.go
@@ -0,0 +1,18 @@
+func search(nums []int, target int) int {
+    
+    low := 0
+    high := len(nums)-1
+    
+    for low <= high{
+        mid := (low+high)/2
+        if nums[mid] == target{
+            return mid
+        }else if target > nums[mid]{
+            low =  mid+1
+        }else{
+            high = mid-1
+        }
+
+    }
+    return -1
+}
