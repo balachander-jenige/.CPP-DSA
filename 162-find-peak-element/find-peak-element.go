5a834f4449c6eff18ef8fcc8cfc917c8bcf3cb2c
Balachander Jenige <eduwithbala@gmail.com>
2026-06-01 06:51:39 +0800

    Time: 0 ms (100.00%) | Memory: 4.6 MB (6.58%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.go
@@ -0,0 +1,26 @@
+func findPeakElement(arr []int) int {
+    
+      // Search space is the entire array
+    low := 0
+    high := len(arr) - 1
+
+    // Continue until only one element remains
+    for low < high {
+        mid := (low + high) / 2
+
+        // If current element is smaller than the next,
+        // we are on the ascending slope of the mountain.
+        // Therefore, the peak must be to the right.
+        if arr[mid] < arr[mid+1] {
+            low = mid + 1
+        } else {
+            // We are on the descending slope or at the peak.
+            // The peak could be mid itself, so keep mid
+            // in the search space.
+            high = mid
+        }
+    }
+
+    // When low == high, both point to the peak index.
+    return low
+}
