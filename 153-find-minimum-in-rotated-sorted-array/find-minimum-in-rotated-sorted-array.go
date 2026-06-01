cff08aba9e600f21b6a0951c93e9885a81181303
Balachander Jenige <eduwithbala@gmail.com>
2026-06-01 08:18:49 +0800

    Time: 0 ms (100.00%) | Memory: 4.4 MB (39.19%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
@@ -1,17 +1,29 @@
 func findMin(nums []int) int {
     n := len(nums)
+
+    // Binary search range
     low := 0
-    high := len(nums)-1
+    high := n - 1
+
+    // Stores index of potential minimum element
     res := 0
 
-    for low <= high{
-        mid := (high+low)/2
-        if nums[mid] > nums[n-1]{
-            low = mid+1
-        }else{
+    for low <= high {
+        mid := (high + low) / 2
+
+        // If middle element is greater than the last element,
+        // it means the minimum is in the right half (rotated part)
+        if nums[mid] > nums[n-1] {
+            low = mid + 1
+        } else {
+            // nums[mid] could be the minimum, so store it
             res = mid
-            high = mid -1
+
+            // Try to find a smaller value on the left side
+            high = mid - 1
         }
     }
+
+    // Return the minimum value found
     return nums[res]
 }
