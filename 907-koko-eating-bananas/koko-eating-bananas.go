1ba2d2eccc67eea5c26754d4df2d0fd10beebf9b
Balachander Jenige <eduwithbala@gmail.com>
2026-06-03 07:25:13 +0800

    Time: 6 ms (47.31%) | Memory: 8.3 MB (82.04%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.go
+++ 907-koko-eating-bananas/koko-eating-bananas.go
@@ -1,27 +1,57 @@
-func fun(piles []int, guess int)int {
+// Returns the total hours needed if Koko eats
+// at a speed of 'guess' bananas per hour.
+func fun(piles []int, guess int) int {
     h := 0
-    for _,v := range piles{
-        h += v/guess
-        if v% guess != 0{
+
+    // Calculate hours required for each pile
+    for _, v := range piles {
+
+        // Add complete hours
+        h += v / guess
+
+        // If there are remaining bananas,
+        // one extra hour is needed
+        if v%guess != 0 {
             h++
         }
     }
+
     return h
 }
+
 func minEatingSpeed(piles []int, h int) int {
+
+    // Minimum possible eating speed is 1 banana/hour
     low := 1
+
+    // Maximum possible eating speed is the largest pile
+    // (finish the largest pile in one hour)
     high := slices.Max(piles)
+
+    // Stores the minimum valid eating speed found so far
     res := -1
-    for low <= high{
-        guess := (low+high)/2
-        minhours := fun(piles,guess)
-        if minhours > h{
-            low = guess +1
-        }else{
+
+    // Binary search on eating speed
+    for low <= high {
+
+        // Candidate eating speed
+        guess := (low + high) / 2
+
+        // Hours needed at this speed
+        minhours := fun(piles, guess)
+
+        // Too slow: need a higher eating speed
+        if minhours > h {
+            low = guess + 1
+        } else {
+
+            // Valid speed found
             res = guess
-            high = guess-1
+
+            // Try to find a smaller valid speed
+            high = guess - 1
         }
     }
+
     return res
-     
 }
